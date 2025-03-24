dcfabafeb5b709ec01920504df10e169bc77ecd9
Aucourantcs-aman <aman.aucourantcs@gmail.com>
2025-03-24 14:31:39 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,24 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import { Button, HStack } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <>
-        <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+      {/* <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header> */}
       <h1 class="text-3xl font-bold underline">Hello Clients</h1>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Provider } from "@/components/ui/provider";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
@@ -15,8 +16,10 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
-    </ClerkProvider>
+    <Provider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <App />
+      </ClerkProvider>
+    </Provider>
   </StrictMode>
 );
